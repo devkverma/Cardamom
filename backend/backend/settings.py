40ef32d2d1ec2944ba67ac3cb47130a9357b3a4a
devkverma <devverma269@gmail.com>
2025-10-22 21:50:35 +0530

    dockerized backend, frontend and postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,10 +82,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
@@ -133,7 +132,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173'
+    "http://localhost:3000"
 ]
 
 
